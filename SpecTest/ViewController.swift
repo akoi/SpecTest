269917b1a02aa74c2eb24c6585e133cdfae293f0
Anton Bagchi <anton.bagchi@vocado.com>
2014-11-18 15:24:43 +0100

    Initial Commit

---
+++ SpecTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpecTest
+//
+//  Created by Anton Bagchi on 18/11/2014.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
