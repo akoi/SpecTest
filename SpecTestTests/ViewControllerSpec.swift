ba4241a91fea26bac055026dea31d1c890f12ca7
Anton Bagchi <anton.bagchi@vocado.com>
2014-11-18 15:32:09 +0100

    Added ViewControllerSpec test

---
+++ SpecTestTests/ViewControllerSpec.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerSpec.swift
+//  SpecTest
+//
+//  Created by Anton Bagchi on 18/11/2014.
+//
+//
+
+import SpecTest
+import UIKit
+import Quick
+import Nimble
+
+class ViewControllerSpecs: QuickSpec {
+    
+    override func spec() {
+        var viewController: ViewController!
+        
+        beforeEach {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            viewController = storyboard.instantiateViewControllerWithIdentifier("ViewController") as ViewController
+        }
+        
+        describe("the view controller after initialization") {
+            it("should be of type ViewController") {
+                expect(viewController.isKindOfClass(ViewController)).to(beTruthy())
+            }
+        }
+    }
+}
+
